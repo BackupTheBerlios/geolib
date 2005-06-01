bbf1e16a069374dafc75d492cee69343600c2f60
claudiocordara <claudiocordara>
2005-06-01 21:45:32 +0000

    Persistency objects interface

---
+++ GeoLib/GEOL_Persistency.h
@@ -0,0 +1,43 @@
+/***************************************************************************
+							 GEOL_Persistency.h
+                             -------------------
+    begin                : 25.03.2005
+    copyright            : (C) 2004 by Claudio Cordara
+    email                : claudiocordara@libero.it
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
+
+#ifndef GEOL_PERSISTENCY_H
+#define GEOL_PERSISTENCY_H
+
+#include <fstream.h>
+
+class GEOL_Persistency {
+public:
+	GEOL_Persistency() {}
+	virtual ~GEOL_Persistency() = 0;
+	
+	//virtual bool Load() = 0;
+	//virtual bool Save() = 0;
+	virtual bool LoadBinary(std::ifstream *theStream) = 0;
+	virtual bool SaveBinary(std::ofstream *theStream) = 0;
+	virtual bool LoadISO(std::ifstream *theStream) = 0;
+private:
+};
+
+
+
+inline GEOL_Persistency::~GEOL_Persistency() {}
+
+
+#endif
